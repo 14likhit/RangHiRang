0e0848c04d2b8252d48c3e31befabe12ddd50e84
likhit <likhit@udhaar.co>
2019-10-03 20:42:42 +0530

    Renamed Main Activity to ColorsListActivity. Updated Manifest and XML.

---
+++ app/src/main/java/com/likhit/ranghirang/ui/home/ColorsListActivity.java
@@ -0,0 +1,15 @@
+package com.likhit.ranghirang.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.likhit.ranghirang.R;
+
+public class ColorsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colors);
+    }
+}
