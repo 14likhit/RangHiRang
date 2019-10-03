9a8b4c7aedaee5cded3136503ff3161d44051702
likhit <likhit@udhaar.co>
2019-10-04 01:01:23 +0530

    [LoginActivity]->Added Activity for login.

---
+++ app/src/main/java/com/likhit/ranghirang/ui/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.likhit.ranghirang.ui.login;
+
+import android.os.Bundle;
+
+import com.likhit.ranghirang.R;
+import com.likhit.ranghirang.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
