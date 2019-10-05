5fc735c29ff612e0f0ca91f2feb5e997dfc32bc5
likhit <likhit@udhaar.co>
2019-10-05 15:00:59 +0530

    [SplashActivity]->Updated Splash Activity.

--- app/src/main/java/com/likhit/ranghirang/ui/splash/SplashActivity.java
+++ app/src/main/java/com/likhit/ranghirang/ui/splash/SplashActivity.java
@@ -1,16 +1,32 @@
 package com.likhit.ranghirang.ui.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.likhit.ranghirang.R;
+import com.likhit.ranghirang.databinding.ActivitySplashBinding;
+import com.likhit.ranghirang.utils.ActivityLauncher;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ActivitySplashBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+        startTimer();
+    }
+
+    private void startTimer() {
+        //30 Sec Timer.
+        binding.getRoot().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ActivityLauncher.launchLoginActivity(SplashActivity.this);
+            }
+        }, 5000);
     }
 }
