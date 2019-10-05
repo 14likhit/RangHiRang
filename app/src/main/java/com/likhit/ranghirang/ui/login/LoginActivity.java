7f3534a55a620a7c26c8004859c656e4add6574f
likhit <likhit@udhaar.co>
2019-10-05 13:08:32 +0530

    [LoginActivity]->Added Basic OnClick Event for LoginButton.

--- app/src/main/java/com/likhit/ranghirang/ui/login/LoginActivity.java
+++ app/src/main/java/com/likhit/ranghirang/ui/login/LoginActivity.java
@@ -1,15 +1,36 @@
 package com.likhit.ranghirang.ui.login;
 
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.databinding.DataBindingUtil;
 
 import com.likhit.ranghirang.R;
 import com.likhit.ranghirang.base.BaseActivity;
+import com.likhit.ranghirang.databinding.ActivityLoginBinding;
+import com.likhit.ranghirang.utils.ActivityLauncher;
 
 public class LoginActivity extends BaseActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        initView();
+    }
+
+    private void initView() {
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityLauncher.launchColorListActivity(LoginActivity.this);
+            }
+        });
+
     }
 }
