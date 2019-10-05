382201009663cf227c2bb2030eaeafa61589920e
likhit <likhit@udhaar.co>
2019-10-05 13:47:02 +0530

    [ColorsListViewModel]->ViewModel Class for Colors List.

---
+++ app/src/main/java/com/likhit/ranghirang/ui/home/ColorsListViewModel.java
@@ -0,0 +1,38 @@
+package com.likhit.ranghirang.ui.home;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import com.likhit.ranghirang.customListener.OnResponseListener;
+import com.likhit.ranghirang.data.model.ColorList;
+import com.likhit.ranghirang.data.remote.ApiClient;
+import com.likhit.ranghirang.data.remote.ApiService;
+import com.likhit.ranghirang.data.remote.RemoteDataSourceClass;
+
+public class ColorsListViewModel extends AndroidViewModel implements OnResponseListener<ColorList> {
+
+    MutableLiveData<ColorList> colorListMutableLiveData = new MutableLiveData<>();
+
+    private RemoteDataSourceClass remoteDataSourceClass;
+
+    public ColorsListViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    private void getColors(int page) {
+        RemoteDataSourceClass.getInstance(ApiClient.getRetrofitInstance().create(ApiService.class)).getColors(page, this);
+    }
+
+    @Override
+    public void onSuccess(ColorList data) {
+        colorListMutableLiveData.setValue(data);
+    }
+
+    @Override
+    public void onFailed(String error) {
+        colorListMutableLiveData.setValue(null);
+    }
+}
