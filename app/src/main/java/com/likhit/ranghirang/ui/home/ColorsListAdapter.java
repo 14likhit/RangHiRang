97a1ae40a3d09e5c300fbf2d2d14fb96617eab9e
likhit <likhit@udhaar.co>
2019-10-05 12:57:12 +0530

    [ColorsListAdapter]->Added layout for the adapter.

--- app/src/main/java/com/likhit/ranghirang/ui/home/ColorsListAdapter.java
+++ app/src/main/java/com/likhit/ranghirang/ui/home/ColorsListAdapter.java
@@ -5,9 +5,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.likhit.ranghirang.R;
 import com.likhit.ranghirang.data.model.Color;
+import com.likhit.ranghirang.databinding.LayoutColorItemBinding;
 
 import java.util.List;
 
@@ -17,28 +20,40 @@ public class ColorsListAdapter extends RecyclerView.Adapter<ColorsListAdapter.Co
 
     private LayoutInflater layoutInflater;
 
+    public void setColors(List<Color> colors) {
+        this.colors = colors;
+    }
+
     @NonNull
     @Override
     public ColorListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        if (layoutInflater == null) {
+            layoutInflater = LayoutInflater.from(parent.getContext());
+        }
+        return new ColorsListAdapter.ColorListViewHolder(layoutInflater.inflate(R.layout.layout_color_item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ColorListViewHolder holder, int position) {
+        Color color = colors.get(position);
 
+        holder.binding.colorTextView.setText(color.getName());
     }
 
     @Override
     public int getItemCount() {
-        if(colors!=null && colors.size()>0){
+        if (colors != null && colors.size() > 0) {
             return colors.size();
         }
         return 0;
     }
 
     public class ColorListViewHolder extends RecyclerView.ViewHolder {
+        private LayoutColorItemBinding binding;
+
         public ColorListViewHolder(@NonNull View itemView) {
             super(itemView);
+            binding = DataBindingUtil.bind(itemView);
         }
     }
 }
