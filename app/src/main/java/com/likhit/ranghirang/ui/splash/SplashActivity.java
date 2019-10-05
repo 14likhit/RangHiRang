40c4f98e293cbe49d6eb05cecd21f1dfb879e4a3
likhit <likhit@udhaar.co>
2019-10-05 14:58:38 +0530

    [SplashActivity]->Splash Activity. [AndroidManifest]->Added SplashActivity.

---
+++ app/src/main/java/com/likhit/ranghirang/ui/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.likhit.ranghirang.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.likhit.ranghirang.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
